8641f5d5dfa4e4288ddfb77e3b34ad91cd91b8db
MEGUMI SHIMABUKURO <shimabukuromeg@gmail.com>
2019-07-15 17:24:28 +0900

    layoutsディレクトリによるレイアウトの共通化

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,5 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        my-first-nuxt-app-chapter03
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <h1>Index page</h1>
 </template>
 
 <script>
